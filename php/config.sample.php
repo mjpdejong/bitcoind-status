d99a6ca304ba1d43ebe351de116a13ab413baa07
Sjon Hortensius <sjon@hortensius.net>
2016-01-16 12:14:40 +0100

    Bugfix where variable was taken from incorrect $data
    also renamed disk to path, since it's not a device you should configure

--- php/config.sample.php
+++ php/config.sample.php
@@ -42,7 +42,7 @@ $config = array(
   // UI
   'display_ip'              => false,
   'display_free_disk_space' => false,
-  'blockchain_disk'         => '.',
+  'blockchain_path'         => '/',
   'display_testnet'         => false,
   'display_version'         => true,
   'display_github_ribbon'   => true,
